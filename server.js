7f6d5d81a7c0ea0e93bb3f867eecaddd7dccc27f
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-05-28 16:17:09 -0400

    clean file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const express = require("express");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
@@ -24,16 +25,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-/*SERVER END POINTS
-root (/) --> res = this is working
-/signin --> POST = success / fail
-/register --> POST = user
-/profile/:userId --> GET = user 
-/image --> PUT --> user */
 app.get("/", (req, res) => {
   res.send("Success")
 });
-
 
 app.post("/signin", signin.handleSignin(db, bcrypt));
 app.post("/register", register.handleRegister(db, bcrypt));
@@ -44,60 +38,3 @@ app.post("/clarifai", clarifai.handleClarifaiCall);
 app.listen(3001, () => {
   console.log("App is running on port 3001");
 });
-
-/* NOTES WHILE DEVELOPING 
-// TESTING FUNCTION
-// FUNCTION: get all users
-// const getAllUsers = () => {
-//   db.select('*').from('users').then(data => {
-//     console.log("Checking All Users In Postgres Database");
-//     console.log(data);
-//   });
-// }
-
-// BCRYPT EXAMPLE
-// let hash = "$2a$10$PiLGCvFzsUcwno2p3fgznejtftUOA51KMo/5tQKhIo1A2WMYT6ZUq";
-
-// bcrypt.compare("apples", hash, function(err, res) {
-//   console.log("First Guess: ", res);
-// });
-
-// bcrypt.compare("veggies", hash, function(err, res) {
-//   console.log("Second Guess: ", res);
-// });
-
-// HARDCODED DATABASES
-// const database = {
-//   users: [
-//     {
-//       id: "123",
-//       name: "John",
-//       email: "john@gmail.com",
-//       password: "cookies",
-//       entries: 0,
-//       joined: new Date()
-//     },
-//     {
-//       id: "124",
-//       name: "Sally",
-//       email: "sally@gmail.com",
-//       password: "bananas",
-//       entries: 0,
-//       joined: new Date()
-//     },
-//   ]
-// }
-
-// FUNCTION: findUserById 
-// CREATED before postgres sql was connected
-// const findUserById = (id, res, callback) => {
-//   const user = database.users.find(user => user.id === id);
-
-
-//   if(user) {
-//     callback(res, user);
-//   } else {
-//     return res.status(400).json("User Not Found");
-//   }
-// }
-*/
