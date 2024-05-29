f67c083db3c00f2d7d923e81781930ba9459ea34
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-05-28 21:56:25 -0400

    debug for connection

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 
+
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
+
 const express = require("express");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
